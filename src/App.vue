d2903a5a3fd1315d12967f0864142b4dfb3480e8
DylanVallejo <dylan1811vallejo@gmail.com>
2023-03-18 23:23:36 -0500

    typos

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="p-14 ">
-    <router-link to="/" class=" justify-center text-4xl text-black-300">Sear Pokemon</router-link>
+    <router-link to="/" class=" justify-center text-4xl text-black-300">Search Pokemon</router-link>
     <!-- <router-link to="/test" class=" ml-10 justify-center text-4xl text-blue-300">Hola</router-link> -->
   </div>
   <router-view/>
